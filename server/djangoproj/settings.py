06acdb8266d2733d9e919f885f81ce13651e7171
VeronikaHromcenkova <veronika.hromcenkova@bertelsmann.de>
2025-11-24 23:13:09 +0200

    Create Django Proxy Services of Backend APIs

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,12 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
+ALLOWED_HOSTS = ['localhost', "https://veronikahrom-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/"]
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000', 
+    "https://veronikahrom-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/",
+    "https://veronikahrom-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
